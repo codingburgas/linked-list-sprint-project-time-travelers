8295fa7a03d014aabc29a4bf47b586cb81e2ce6c
VBBoyanov22 <VBBoyanov22@codingburgas.bg>
2025-03-29 23:23:38 +0200

    Add static libraries for Business Logic Layer (BLL) and Data Access Layer (DAL). Edit premake.

--- premake5.lua
+++ premake5.lua
@@ -1,32 +1,82 @@
 workspace "TimeTravelers"
-    startproject "TimeTravelers"
+    startproject "PL"
     architecture "x86_64"
     configurations { "Debug", "Release" }
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-project "TimeTravelers"
-    location "TimeTravelers"
+-- Presentation Layer (PL)
+project "PL"
+    location "TimeTravelers/PL"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++20"
 
-    targetdir ("./bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("./bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files { "./%{prj.name}/src/**.cpp", "./%{prj.name}/include/**.h", "./%{prj.name}/assets/**.png"}
+    files { "./TimeTravelers/PL/src/**.cpp", "./TimeTravelers/PL/include/**.h", "TimeTravelers/PL/assets/**.png" }
+    
+    includedirs { "./TimeTravelers/PL/include",  "./TimeTravelers/BLL/include", "./TimeTravelers/DAL/include" }
 
-    includedirs { "./%{prj.name}/src", "./%{prj.name}/include" }
-
-    libdirs {"./%{prj.name}/include" }
+    links { "TimeTravelers/BLL", "TimeTravelers/DAL" }
 
     staticruntime "On"
     systemversion "latest"
 
     filter "configurations:Debug"
-        defines "OD_DEBUG"
+        defines "PL_DEBUG"
         symbols "On"
 
     filter "configurations:Release"
-        defines "OD_RELEASE"
+        defines "PL_RELEASE"
+        optimize "On"
+
+-- Business Logic Layer (BLL)
+project "BLL"
+    location "TimeTravelers/BLL"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files { "./TimeTravelers/BLL/src/**.cpp", "./TimeTravelers/BLL/include/**.h" }
+
+    includedirs { "./TimeTravelers/BLL/include" }
+
+    staticruntime "On"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "BLL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "BLL_RELEASE"
+        optimize "On"
+
+-- Data Access Layer (DAL)
+project "DAL"
+    location "TimeTravelers/DAL"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files { "./TimeTravelers/DAL/src/**.cpp", "./TimeTravelers/DAL/include/**.h" }
+    includedirs { "./TimeTravelers/DAL/include" }
+
+    staticruntime "On"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "DAL_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "DAL_RELEASE"
         optimize "On"
