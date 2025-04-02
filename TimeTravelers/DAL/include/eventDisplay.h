dee9c52f443cba75a9637fa31601abf3cd2a8f21
aavezirova22 <aavezirova22@codingburgas.bg>
2025-04-02 16:05:32 +0300

    Add files for browsing events

---
+++ TimeTravelers/DAL/include/eventDisplay.h
@@ -0,0 +1,8 @@
+//
+// Created by Ani Vezirova on 2.04.25.
+//
+
+#ifndef EVENTDISPLAY_H
+#define EVENTDISPLAY_H
+
+#endif //EVENTDISPLAY_H
---
+++ TimeTravelers/DAL/src/eventDisplay.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Ani Vezirova on 2.04.25.
+//
