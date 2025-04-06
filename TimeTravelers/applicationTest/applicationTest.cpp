62e18970c586bb128cd4323649ec91d6fd31f21a
niribareva22 <niribareva22@codingburgas.bg>
2025-04-06 18:56:20 +0300

    Add loginTest to test login function

--- TimeTravelers/applicationTest/applicationTest.cpp
+++ TimeTravelers/applicationTest/applicationTest.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../BLL/include/register.h"
+#include "../BLL/include/login.h"
+#include "../DAL/include/files.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,16 +14,36 @@ namespace applicationTest
 		
 		TEST_METHOD(registerTest)
 		{
+			json empty;
+			FileManager::saveJSON("users.json", empty);
+
 			std::string username = "testUser";
 			std::string password = "password123#";
 			
 			
 			bool firstAttempt = RegisterManager::registerUser(username, password);
-			bool secondAttempt = RegisterManager::registerUser(username, "testPassword12#");
+			bool secondAttempt = RegisterManager::registerUser(username, "testPass12#");
 
 			Assert::IsTrue(firstAttempt);
 			Assert::IsFalse(secondAttempt);
 
+		}
+
+		TEST_METHOD(loginTest)
+		{
+			json empty;
+			FileManager::saveJSON("users.json", empty);
+
+			std::string username = "loginUser";
+			std::string password = "loginPass!123";
+
+			RegisterManager::registerUser(username, password);
+
+			bool loginSuccess = LoginManager::login(username, password);
+			Assert::IsTrue(loginSuccess);
+
+			bool loginFail = LoginManager::login(username, "wrongPass12!");
+			Assert::IsFalse(loginFail);
 		}
 	};
 }
